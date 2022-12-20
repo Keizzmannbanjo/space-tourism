9dd3c30466b39ab03359fb2d7230a6269e35ecf3
KeizzmannBanjo <keizzmannbanjo@gmail.com>
2022-12-20 10:16:47 +0100

    Created-basic-page-structure

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -23,16 +23,16 @@ function Destination() {
             <li>Europa</li>
             <li>Titan</li>
           </DestinationNav>
-          <div>
-            <h1>Moon</h1>
-            <p>
+          <DestinationFacts>
+            <DestinationName>Moon</DestinationName>
+            <Details>
               See our planet as you’ve never seen it before. A perfect relaxing
-              trip away to help regain perspective and come back refreshed.
-              While you’re there, take in some history by visiting the Luna 2
-              and Apollo 11 landing sites.
-            </p>
+              trip away to help regain perspective and come back refreshed.While
+              you’re there, take in some history by visiting the Luna2 and
+              Apollo 11 landing sites.
+            </Details>
             <hr />
-            <div>
+            <MoreDetailsContainer>
               <div>
                 <p>Avg. Distance</p>
                 <h4>384,400km</h4>
@@ -41,8 +41,8 @@ function Destination() {
                 <p>Est. Travel Time</p>
                 <h4>3 Days</h4>
               </div>
-            </div>
-          </div>
+            </MoreDetailsContainer>
+          </DestinationFacts>
         </DestinationFactContainer>
       </MainContainer>
     </Wrapper>
@@ -57,6 +57,7 @@ const Wrapper = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   min-height: 100vh;
+  padding-bottom: 1rem;
 `;
 
 const MainContainer = styled.div`
@@ -89,11 +90,40 @@ const DestinationNav = styled.ul`
     color: var(--color-light);
     border-bottom: 2px solid transparent;
     cursor: pointer;
-    padding:5px;
+    padding: 5px;
   }
 
   li:hover {
     border-bottom: 2px solid var(--color-light);
+  }
+`;
+
+const DestinationName = styled.h2`
+  text-transform: uppercase;
+  margin: 2rem 0rem 0.9rem;
+`;
+
+const Details = styled.p`
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+`;
+
+const DestinationFacts = styled.div`
+  hr {
+    margin: 3.5rem 0 2rem;
+    border-color: var(--color-grey);
+    border-width: 2px;
+  }
+`;
+
+const MoreDetailsContainer = styled.div`
+  display: flex;
+  width: 70%;
+  justify-content: space-between;
+
+  h4 {
+    margin: 1rem 0;
   }
 `;
 
